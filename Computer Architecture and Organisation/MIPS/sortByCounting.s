140a237dc1b7d703018363d7f460d86dd81a4b7d
Erwin de Haan <git@erayan.eu>
2015-02-27 10:28:50 +0100

    Fixed some address errors.

--- Computer Architecture and Organisation/MIPS/sortByCounting.s
+++ Computer Architecture and Organisation/MIPS/sortByCounting.s
@@ -22,32 +22,32 @@ sortByCounting:	subi $sp, $sp, 4
 for_sort:	move $a2, $s0 # pass i as thrid argument
 		jal countLessThen # call result is in $v0
 		
-		sll $t0, $v0, 4 # get lessThan address result
+		sll $t0, $v0, 2 # get lessThan address result
 		add $t0, $t0, $s2 # $t0 now holds the current target element's address in b (b[lessThan])
 		
-		sll $t1, $s0, 4 # get i address result
+		sll $t1, $s0, 2 # get i address result
 		add $t1, $t1, $a0 # $t1 now holds the current source element's address in a (a[i])
 		
-		lw $t2, ($t1)
-		sw $t2, ($t0)
+		lw $t2, ($t1) # load source element
+		sw $t2, ($t0) # store source to target
 		
 		addi $s0,$s0,1 # increase counter
 		bne $a1,$s0,for_sort # if i is not equal to length rerun loop
 		
 for_copy:	move $s0, $zero # set $s0 to zero (i)
-		sll $t0, $s0, 4 # get i address result
+		sll $t0, $s0, 2 # get i address result
 		
 		add $t0, $t0, $s2 # $t0 now holds the current target element's address in b (b[lessThan])
 		add $t1, $t0, $a0 # $t1 now holds the current source element's address in a (a[i])
 		
-		lw $t2, ($t1)
-		sw $t2, ($t0)
+		lw $t2, ($t1) # load source element
+		sw $t2, ($t0) # store source to target
 		
 		addi $s0,$s0,1 # increase counter
 		bne $a1,$s0,for_copy # if i is not equal to length rerun loop
 
 		lw $s0 ($sp) # i
-		subi $sp, $sp, 4		
+		subi $sp, $sp, 2		
 		lw $s1 ($sp) # i
 		subi $sp, $sp, 4
 		lw $s2 ($sp) # i
